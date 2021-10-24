ec2382f4af8a16df10f66580f8e040aa6bdcdf82
realaravinth <realaravinth@batsense.net>
2021-10-24 07:16:39 +0530

    fix SQL syntax err

--- migrations/20211023_01_0W52q-event-subscriptions.py
+++ migrations/20211023_01_0W52q-event-subscriptions.py
@@ -19,12 +19,12 @@ steps = [
         CREATE TABLE IF NOT EXISTS interface_interfaces(
             url VARCHAR(3000) UNIQUE NOT NULL,
             ID INTEGER PRIMARY KEY NOT NULL
-        )
+        );
     """),
     step("""
         CREATE TABLE IF NOT EXISTS interface_event_subscriptsions(
             repository_id INTEGER NOT NULL REFERENCES interface_repositories(ID) ON DELETE CASCADE,
-            interface_id INTEGER NOT NULL REFERENCES interface_interfaces(ID) ON DELETE CASCADE,
+            interface_id INTEGER NOT NULL REFERENCES interface_interfaces(ID) ON DELETE CASCADE
         );
     """)
 ]
