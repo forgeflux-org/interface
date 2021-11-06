c3ba014c9affbcd7b9a5d55182a99b76d1ccf433
realaravinth <realaravinth@batsense.net>
2021-11-06 18:45:29 +0530

    test for System
    Run tests implemented on Repo but with proxied System methods

--- libgit/src/system.rs
+++ libgit/src/system.rs
@@ -75,7 +75,7 @@ impl System {
         })
     }
 
-    pub fn init_repo(&self, local: String, upstream: String) -> FResult<()> {
+    pub fn init_repo(&self, local: String, upstream: String) -> FResult<Repo> {
         let lock = Lock::new(false);
         self.lock
             .insert(local.as_bytes(), bincode::serialize(&lock).unwrap())
@@ -85,8 +85,7 @@ impl System {
             .insert(local.as_bytes(), upstream.as_bytes())
             .unwrap();
 
-        Repo::new(&self.base, local, upstream)?;
-        Ok(())
+        Ok(Repo::new(&self.base, local, upstream)?)
     }
 
     pub fn with_local(&self, local: &str) -> FResult<Option<Repo>> {
@@ -185,5 +184,119 @@ fn run_with_repo<T, F: FnOnce() -> T>(sys: &System, repo: &Repo, f: F) -> T {
         // that the repository is available before the first one does, causing a race
         // condition.
         unimplemented!("When repo is locked")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::*;
+    use crate::*;
+
+    use std::env;
+
+    #[test]
+    fn system_works() {
+        const NAME: &str = "SYSTEM_WORKS";
+        const UPSTREAM: &str = "https://github.com/realaravinth/actix-auth-middleware";
+        let tmp = env::temp_dir();
+        let base_dir = tmp.join(NAME);
+        let local = base_dir.join("local");
+        let _ = fs::remove_dir_all(&base_dir);
+        fs::create_dir_all(&local).unwrap();
+
+        let local_bare_repo = Repository::init_bare(&local).unwrap();
+        let sys = System::new(base_dir.as_os_str().to_str().unwrap()).unwrap();
+        let local_repo = sys
+            .init_repo(
+                local.as_os_str().to_str().unwrap().to_owned(),
+                UPSTREAM.into(),
+            )
+            .unwrap();
+
+        {
+            let check_equal = |repo: &Repo| {
+                assert_eq!(repo.path, local_repo.path);
+                assert_eq!(repo.upstream, local_repo.upstream);
+                assert_eq!(repo.local, local_repo.local);
+            };
+            let with_local = sys
+                .with_local(local.as_os_str().to_str().unwrap())
+                .unwrap()
+                .unwrap();
+            check_equal(&with_local);
+
+            let with_upstream = sys.with_upstream(UPSTREAM).unwrap().unwrap();
+            check_equal(&with_upstream);
+
+            assert!(sys.with_upstream(NAME).unwrap().is_none());
+            assert!(sys.with_local(UPSTREAM).unwrap().is_none());
+        }
+
+        sys.fetch_upstream(&local_repo).unwrap();
+
+        let default_branch = local_repo.default_branch().unwrap();
+        sys.push_local(&local_repo, &default_branch).unwrap();
+
+        assert_eq!(
+            local_repo.repo.head().unwrap().peel_to_tree().unwrap().id(),
+            local_bare_repo
+                .head()
+                .unwrap()
+                .peel_to_tree()
+                .unwrap()
+                .as_object()
+                .id(),
+        );
+
+        let patch = make_changes(&local_repo, NAME);
+        println!("{}", patch);
+        let processed_patch = sys.process_patch(&local_repo, patch, NAME.into()).unwrap();
+        println!("{}", processed_patch);
+        assert!(!processed_patch.contains(IGNORE_FILE));
+        assert!(!processed_patch.contains("foo"));
+        assert!(!processed_patch.contains("bar"));
+        assert!(processed_patch.contains("lib.rs"));
+        assert!(processed_patch.contains("README.md"));
+
+        let patch = Patch::new(
+            NAME.into(),
+            AUTHOR.into(),
+            AUTHOR_EMAIL.into(),
+            processed_patch,
+        )
+        .unwrap();
+
+        let admin = InterfaceAdmin::new(AUTHOR_EMAIL.into(), AUTHOR.into()).unwrap();
+
+        sys.apply_patch(&local_repo, patch, &admin, NAME.into())
+            .unwrap();
+        let tmp_branch = local_repo.repo.find_reference(&get_ref(NAME)).unwrap();
+
+        let mut checkout_options = CheckoutBuilder::new();
+        checkout_options.force();
+        let branch_head_obj = tmp_branch.peel(ObjectType::Tree).unwrap();
+
+        local_repo
+            .repo
+            .checkout_tree(&branch_head_obj, Some(&mut checkout_options))
+            .unwrap();
+        local_repo.repo.set_head(&get_ref(NAME)).unwrap();
+
+        assert!(fs::read_to_string(local_repo.path.join("src/lib.rs"))
+            .unwrap()
+            .contains("change lib.rs"));
+
+        assert!(fs::read_to_string(local_repo.path.join("README.md"))
+            .unwrap()
+            .contains("change readme"));
+
+        local_repo.push_local(NAME).unwrap();
+        let tmp_branch_remote = local_bare_repo.find_reference(&get_ref(NAME)).unwrap();
+
+        assert_eq!(
+            local_repo.repo.head().unwrap().peel_to_tree().unwrap().id(),
+            tmp_branch_remote.peel_to_tree().unwrap().as_object().id(),
+        );
     }
 }
