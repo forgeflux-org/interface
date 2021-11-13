2705d3929957f7a085f748feb029472c71d68f48
realaravinth <realaravinth@batsense.net>
2021-11-13 19:11:08 +0530

    rename repository table

--- migrations/20211023_01_0W52q-event-subscriptions.py
+++ migrations/20211023_01_0W52q-event-subscriptions.py
@@ -7,8 +7,9 @@ from yoyo import step
 __depends__ = {}
 
 steps = [
+        ## local repositories: repositories on the forge that this interface services
     step("""
-    CREATE TABLE IF NOT EXISTS interface_repositories(
+    CREATE TABLE IF NOT EXISTS interface_local_repositories(
         html_url VARCHAR(3000) UNIQUE NOT NULL,
         ID INTEGER PRIMARY KEY NOT NULL
     );
@@ -22,7 +23,7 @@ steps = [
     """),
     step("""
         CREATE TABLE IF NOT EXISTS interface_event_subscriptsions(
-            repository_id INTEGER NOT NULL REFERENCES interface_repositories(ID) ON DELETE CASCADE,
+            repository_id INTEGER NOT NULL REFERENCES interface_local_repositories(ID) ON DELETE CASCADE,
             interface_id INTEGER NOT NULL REFERENCES interface_interfaces(ID) ON DELETE CASCADE
         );
     """)
