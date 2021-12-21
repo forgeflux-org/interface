7bc36ab8f6ce1e69714fdc753fa7f8758bc6f5b4
realaravinth <realaravinth@batsense.net>
2021-12-21 13:33:50 +0530

    Define F_D_INVALID_ISSUE_URL:     this error is returned when the issue URL supplied doesn't conform     to /{owner}/{repo}/issues/.* format

--- interface/forges/base.py
+++ interface/forges/base.py
@@ -130,3 +130,9 @@ F_D_REPOSITORY_EXISTS = Error(
     error="Forge reports repository with the same name already exists",
     status=409,
 )
+
+F_D_INVALID_ISSUE_URL = Error(
+    errcode="F_D_INVALID_ISSUE_URL",
+    error="Issue URL provided is not valid",
+    status=400,
+)
