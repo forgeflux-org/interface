65cf7b058b51eac0ce73f7c3a6eee2281eb30ec1
dat-adi <dat.adithya@gmail.com>
2022-02-09 13:33:25 +0530

    Ignored secrets and black format setup.py

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='interface',
-    version='0.1.0',
+    name="interface",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask[async]',
+        "flask[async]",
     ],
 )
