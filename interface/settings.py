11ea3597da4a0d45542a19140b9adbd1df5fb198
dat-adi <dat.adithya@gmail.com>
2022-02-10 13:32:18 +0530

    Updated lint subroutine for setup.py

--- interface/settings.py
+++ interface/settings.py
@@ -3,7 +3,5 @@ import socket
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
-    envvar_prefix="INTERFACE",
-    environments=True,
-    settings_files=["settings.toml"]
+    envvar_prefix="INTERFACE", environments=True, settings_files=["settings.toml"]
 )
